a1c4b75c1a2dcd99cdb6c1dc8317d3cdb0ec1146
dan blanco <blueseph@gmail.com>
2018-04-10 01:48:32 -0700

    nevermind that was a bad idea

--- src/framework/roti.js
+++ src/framework/roti.js
@@ -1,10 +1,14 @@
+const AWS = require('aws-sdk');
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
 const get = require('./calls/get');
 const list = require('./calls/list');
 const post = require('./calls/post');
 const put = require('./calls/put');
 const del = require('./calls/delete');
 
-const roti = dynamoDb => table => ({
+const roti = table => ({
   get: get(dynamoDb)(table),
   list: list(dynamoDb)(table),
   post: post(dynamoDb)(table),
