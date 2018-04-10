204fd209b02db4c8d3d6618bee8e7f83b01a23e0
dan blanco <blueseph@gmail.com>
2018-04-10 00:22:21 -0700

    roti now requires a ddb instance

--- src/framework/roti.js
+++ src/framework/roti.js
@@ -1,14 +1,10 @@
-const AWS = require('aws-sdk');
-
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
 const get = require('./calls/get');
 const list = require('./calls/list');
 const post = require('./calls/post');
 const put = require('./calls/put');
 const del = require('./calls/delete');
 
-const roti = table => ({
+const roti = dynamoDb => table => ({
   get: get(dynamoDb)(table),
   list: list(dynamoDb)(table),
   post: post(dynamoDb)(table),
